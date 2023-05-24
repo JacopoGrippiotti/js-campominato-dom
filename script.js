7743febf4dce682ae42fd018606963017bff1aff
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-24 16:35:50 +0200

    fixed script.js file to display red color in cells when a bomb is clicked

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ const button = document.getElementById('buttonPlay')
 
 button.append('play')
 
+
+function randomNumber(max, min){
+
+    let randomNumber = Math.floor(Math.random()*(max - min + 1) + min)
+
+    return randomNumber
+}
+
+
+function randomNumbersArray (max, min, elements){
+
+    let array = []
+
+    while(array.length < elements) {
+        
+        let random = randomNumber(max,min)
+        
+        if(!array.includes(random)){
+            
+            array.push(random)
+        }
+        
+    }
+
+    return array
+}
+
 function createDivElement(tagname,classname,content) {
 
     let divElement = document.createElement(tagname)
@@ -17,20 +44,34 @@ button.addEventListener('click', function(){
 
     const gridContainer = document.getElementById('gridContainer')
     
+    gridContainer.classList.add('gridBorder')
+    
+    const bombList = randomNumbersArray(100, 1, 16)
+    
     gridContainer.innerHTML = ""
 
     for (let i = 1; i < 101; i++ ) {
 
-        gridContainer.classList.add('gridBorder')
+        
         
         let gridItem = createDivElement('div', 'gridItem', i)
-    
-        gridItem.addEventListener('click' , function (){
-    
-            gridItem.classList.toggle('aqua')
 
-            console.log(i)
-        })
+        if(!bombList.includes(i)){
+
+            gridItem.addEventListener('click' , function (){
+    
+                gridItem.classList.toggle('aqua')
+    
+                console.log(i)
+            }) 
+        }else{
+
+            gridItem.addEventListener('click', function(){
+
+                gridItem.classList.toggle('red')
+            })
+        }
+        
         
         gridContainer.appendChild(gridItem)
     
