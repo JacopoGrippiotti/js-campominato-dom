10fba28ee4712b0226574e32e9f3fdb5825c78ad
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-24 15:47:24 +0200

    added index.html file script.js file and style.css file

---
+++ script.js
@@ -0,0 +1,44 @@
+const button = document.getElementById('buttonPlay')
+
+button.append('play')
+
+function createDivElement(tagname,classname,content) {
+
+    let divElement = document.createElement(tagname)
+
+    divElement.append(content)
+
+    divElement.classList.add(classname)
+
+    return divElement
+}
+
+button.addEventListener('click', function(){
+
+    const gridContainer = document.getElementById('gridContainer')
+    
+    gridContainer.innerHTML = ""
+
+    for (let i = 1; i < 101; i++ ) {
+
+        gridContainer.classList.add('gridBorder')
+        
+        let gridItem = createDivElement('div', 'gridItem', i)
+    
+        gridItem.addEventListener('click' , function (){
+    
+            gridItem.classList.toggle('aqua')
+
+            console.log(i)
+        })
+        
+        gridContainer.appendChild(gridItem)
+    
+    }
+})
+
+
+
+
+
+
